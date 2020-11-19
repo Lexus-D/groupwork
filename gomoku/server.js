5f5eaf5df72568765592aca2d10563579e418445
syno26 <someiyoshino3137@gmail.com>
2020-11-20 01:34:04 +0900

    石の共有機能の追加（人数制限未実装）

--- gomoku/server.js
+++ gomoku/server.js
@@ -1,27 +1,75 @@
 //サーバーたてて、socketでデータの受け渡し
 
-//サーバーを立てる
-//htmlファイルを読み取る
-//socket.ioを呼び出す
-var http = require('http').createServer(handler);
-var html = require('fs').readFileSync('public/index.html');
-var io = require('socket.io')(http);
+// モジュール
+const http     = require('http');
+const express  = require('express');
+const socketIO = require('socket.io');
 
-function handler(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end(html);
-}
+// オブジェクト
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
-//接続中の処理
-io.sockets.on('connection',function(socket){
+// 定数
+const PORT = 5500;
 
-    //接続切断処理
-    socket.on('disconnect',function(){
-        console.log("disconnect");
-    });
+// グローバル変数
+let iCountUser=0;
+var idCheck=new Array(2);
 
-    //たぶんここに処理を色々書いていくはず
-
+server.listen(PORT,()=>{
+    console.log('server starts on port: %d',PORT);
+    app.use(express.static(__dirname+'/public'));
 })
 
-http.listen(5500);
+//最初の接続の処理
+io.once('connection',socket=>{
+    socket.emit('message',0);
+});
+
+//クライアントの処理
+io.on('connection',socket=>{
+
+    // 人数の確認
+    iCountUser=iCountUser+1;
+    console.log(iCountUser);
+
+    socket.on('message',msg=>{
+        // 調整中
+        /*
+        if(iCountUser<3 && msg[0]==-1 && msg[1]==-1)
+        {
+            var id = socket.id;
+            idCheck[iCountUser-1]=id;
+            msg[3]=id;
+            console.log(id);
+            io.to(id).emit('personal',msg);
+            console.log("tyakka");
+        }
+        else if(msg[3]==idCheck[0])
+        {
+            msg[3]==idCheck[1];
+            io.to(idCheck[1]).emit('personal',msg);
+            console.log(idCheck[1]);
+            console.log("oita");
+        }
+        else if(msg[3]==idCheck[1])
+        {
+            msg[3]==idCheck[0];
+            io.to(idCheck[0]).emit('personal',msg);
+            console.log(idCheck[0]);
+            console.log("oita");
+        }
+        console.log("passed");
+        */
+        io.emit('message',msg);
+    });
+
+    // 切断時の処理
+    socket.on('disconnect',()=>{
+    console.log('disconnect');
+    iCountUser=iCountUser-1;
+    console.log(iCountUser);
+    });
+
+});
