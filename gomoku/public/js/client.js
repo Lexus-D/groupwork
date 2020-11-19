0d6b6757a92680dd03d6d953ea1d38fde176bfea
成田省吾 <shougo@naritashougonoMacBook-Pro.local>
2020-11-19 14:50:41 +0900

    fix
    html内のjsコードをclient.jsに移植

--- gomoku/public/js/client.js
+++ gomoku/public/js/client.js
@@ -34,3 +34,67 @@ socket.on('connect' ,function (data) {//コネクションの接続
         socket.disconnect()
     });
 });
+var imageboard=document.getElementById('imageboard');
+var lineboard=document.getElementById('lineboard');
+var stoneboard=document.getElementById('stoneboard');
+var myturn=0;//1
+var mycolor=1//null
+window.onload=()=>{
+    var imgcontext=imageboard.getContext('2d');
+    var img=new Image();
+    img.src="../image/boardimg.jpg"
+    img.onload=()=>{
+        imgcontext.drawImage(img,0,0);
+    }
+}
+
+const width=imageboard.clientWidth;
+const height=imageboard.clientHeight;
+imageboard.width=600;
+imageboard.height=600;
+lineboard.width=600;
+lineboard.height=600;
+stoneboard.width=600;
+stoneboard.height=600;
+
+
+var context = lineboard.getContext('2d');
+
+for(let i=0;i<=14;i++){
+    context.beginPath();
+    context.moveTo(20+i*40,20);
+    context.lineTo(20+i*40,580);
+    context.stroke();
+}
+for(let i=0;i<=14;i++){
+    context.beginPath();
+    context.moveTo(20,20+i*40);
+    context.lineTo(580,20+i*40);
+    context.stroke();
+}
+var stonecontext=stoneboard.getContext('2d');
+function　drawcircle(x,y,color){
+    if(color){
+        stonecontext.fillStyle="#ffffff";
+    }else{
+        stonecontext.fillStyle="#000000"
+    }
+    stonecontext.beginPath();
+    stonecontext.arc(x,y,10,0,2*Math.PI,true);
+    stonecontext.fill();
+    stonecontext.stroke();
+}
+
+stoneboard.addEventListener('click',(event)=>{
+    if(myturn){
+        return;
+    }
+    myturn=1;
+    var rect=stoneboard.getBoundingClientRect();
+    var x=event.clientX-Math.floor(rect.left);
+    var y=event.clientY-Math.floor(rect.top);
+    x=Math.floor(x/40);
+    y=Math.floor(y/40);
+    console.log(x,y);
+    drawcircle(x,y,mycolor);
+});
