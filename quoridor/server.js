e7b17cd3393376cfc52910676774414f375533f2
syno26 <someiyoshino3137@gmail.com>
2020-12-15 17:37:12 +0900

    石と壁を置いた場合の処理を追加

--- quoridor/server.js
+++ quoridor/server.js
@@ -2,6 +2,7 @@
 const http     = require('http');
 const express  = require('express');
 const socketIO = require('socket.io');
+const { count } = require('console');
 
 // オブジェクト
 const app = express();
@@ -9,16 +10,22 @@ const server = http.Server(app);
 const io = socketIO(server);
 
 // 定数
-const PORT = 5500;
+const PORT = 5500; // ポート番号
 const LENGTH = 9; // 奇数のみ
+const ROOMMAX = 20; // 部屋の最大数
+const PLAYERNUM = 2; // プレイヤーの数 2or4
 
 // グローバル変数
 var stoneBoard=[];
 var wallBoardVertical=[];
 var wallBoardHorizontal=[];
 
+var countRoomUsers = Array(ROOMMAX).fill(0);
+var countRooms = 0;
+
+
 // kはroomの数．上限はとりあえず20にしておく
-for(var k=0;k<20;k++){
+for(var k=0;k<ROOMMAX;k++){
     // ボードを部屋数分作成
     stoneBoard[k]=[];
     wallBoardVertical[k]=[];
@@ -29,10 +36,9 @@ for(var k=0;k<20;k++){
         stoneBoard[k][i]=[];
         for(var j=0;j<9;j++){
             stoneBoard[k][i][j]={
-                state:false,
-                // colorがプレイヤーを表す
-                // 0は石がない意味
-                color:0
+                state:false, // 石が置いてあるかどうか
+                color:0 // colorがプレイヤーを表す
+                        // 0は石がない意味
             }
         }
     }
@@ -59,34 +65,79 @@ server.listen(PORT,()=>{
     app.use(express.static(__dirname+'/public'));
 })
 
-// 最初の接続の処理
-// ここで部屋割りと部屋内での上限を超えていないか調べる
-// ID割り振り，色の設定も
+/*
 io.once('connection',socket=>{
-    //socket.emit('message',0);
 });
+*/
 
 io.on('connection',socket=>{
 
+    // 最初の接続の処理
+    // ここで部屋割りと部屋内での上限を超えていないか調べる
+    // ID割り振り，色の設定も
+
+    // 来た人順で部屋にいれ，部屋がいっぱいになったら次の部屋へ
+    countRoomUsers[countRooms] = countRoomUsers[countRooms] + 1;
+    socket.join(countRooms);
+
+    // ID割り振り
+    var userID = socket.id;
+    var settingInfo={
+        id: userID,
+        color: countRoomUsers[countRooms]%2,
+        room: countRooms
+    }
+
+    // PLAYNUMだけ入ったら次の部屋へ
+    if(countRoomUsers[countRooms]==PLAYERNUM){
+        countRooms = countRooms + 1;
+    }
+
+    // アクセスしてきたclientに設定データを送る
+    io.to(userID).emit('setting',settingInfo);
+    
     // 人数が揃うまで何もできないようにする
     // 人数が揃ったら開始
+    // とりあえず2人用なので特になし
     // io.to().emit('',);
 
-    // ターンが回ってきたら動作できるようにする
-    // socket.on('',=>{
-    //     io.to().emit('',);
-    // })
+    // 石や壁をおけるかどうかの判断はclient側だけでいいかもしれない
 
-    // 動作（石・壁を動かす置く等）に応じてそれが可能か確かめる
-    // ↓動作可能か確かめるアルゴリズム
-    // socket.on('',=>{
+    // 石を置いた場合の処理
+    socket.on('stone',putStone=>{
 
-        // 動作可能ならその結果を部屋全員に送信する
-        // io.to().emit('',);
+        var x=putStone.stone[0];
+        var y=putStone.stone[1];
+        var color=putStone.stone[2];
 
-        // ↓動作の結果が勝敗に繋がっているか確認するアルゴリズム
+        // if(!stoneBoard[putStone.room][x][y].state){
+            stoneBoard[putStone.room][x][y].color=color;
+            stoneBoard[putStone.room][x][y].state=true;
+            io.to(putStone.room).emit('Broadcast',putStone.stone);
+        // }
 
-    // })
+        // ↓勝利条件を満たしているかを判断する関数
+
+    })
+
+    // 壁を置いた場合の処理
+    socket.on('wall',putWall=>{
+
+        var x=putWall.wall[0];
+        var y=putWall.wall[1];
+        // 縦壁か横壁かの情報をwall[2]に入れてほしいです
+        var wallDirection=putWall.wall[2];
+        
+        // 横なら
+        if(wallDirection){
+            wallBoardHorizontal[putWall.room][x][y].state=true;
+            wallBoardHorizontal[putWall.room][x+1][y].state=true;
+        }
+        else{
+            wallBoardVertical[putWall.room][x][y].state=true;
+            wallBoardVertical[putWall.room][x][y+1].state=true;
+        }
+    })
 
     // 接続が切れた場合，試合中断
     // ボードを初期化して再接続を待つ
@@ -96,7 +147,5 @@ io.on('connection',socket=>{
     })
     */
 })
-
-// 動作可能か確かめるアルゴリズム
 
 // 勝敗判定のアルゴリズム
